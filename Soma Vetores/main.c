86ab344d8c8694a33326e3bc91ea0c6ac630eda7
Matheus Machado <103153379+Matheusmachado01@users.noreply.github.com>
2022-07-06 16:42:57 -0300

    Soma Vetores

---
+++ Soma Vetores/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main ()
+{
+
+    int N;
+    double soma, media;
+
+    printf ("Quantos numeros voce vai digitar? ");
+    scanf ("%d", &N);
+
+    double vet[N];
+
+    for (int i = 0; i < N; i++)
+    {
+        printf ("Digite um numero: ");
+        scanf ("%lf", &vet[i]);
+    }
+    printf ("\n");
+    printf ("\Valores: ");
+    for (int i = 0; i < N; i++)
+    {
+        printf ("%.1lf ", vet[i]);
+
+    }
+    printf ("\n");
+
+    soma = 0;
+    for (int i =0; i < N; i++)
+    {
+        soma = soma + vet[i];
+    }
+
+    printf ("SOMA = %.2lf\n", soma);
+    media = soma / N;
+    printf ("MEDIA = %.1lf", media);
+
+    return 0;
+}
